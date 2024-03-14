7297d2ed294804674bb385030a567e1c2453179f
taltonji2 <timaltonji@protonmail.com>
2024-03-13 21:57:49 -0400

    fix A*

--- astar.py
+++ astar.py
@@ -11,32 +11,16 @@ from typing import Callable, Tuple
     #           g(n) + h(n)
 
 class Vertex:
-    def __init__(self, x, y, g=0, h=0, parent=None):
+    def __init__(self, x, y, astar):
         self.x = x
         self.y = y
-        self.g = g
-        self.h = h
-        self.parent = parent if parent is not None else self
-        
-    def calculate_g(self, current, successor) -> None:
-        if (successor.x < current.x and successor.y == current.y): #Successor is to the left
-            self.g = current.g + 1
-        if (successor.x > current.x and successor.y == current.y): #Successor is to the right
-            self.g = current.g + 1
-        if (successor.x == current.x and successor.y < current.y): #Successor is above
-            self.g = current.g + 1
-        if (successor.x == current.x and successor.y > current.y): #Successor is under
-            self.g = current.g + 1
-        self.g = current.g + math.sqrt(2); #diagonal
-
-    def calculate_h(self, goal) -> None: 
-        self.h = math.sqrt(2) * \
-        min(abs(self.x - goal.x), abs(self.y - goal.y)) + \
-        max(abs(self.x - goal.x), abs(self.y - goal.y)) - \
-        min(abs(self.x - goal.x), abs(self.y - goal.y))
+        self.astar = astar
     
     def __lt__(self, other):
-        return self.g  < other.g
+        self_g = self.astar.g_score[self]
+        other_g = self.astar.g_score[other]
+        print("a", self_g, other_g)
+        return self_g < other_g  
 
     def print_coordinates(self) -> None:
         print(f"coordinates ", self.x, self.y)
@@ -44,146 +28,141 @@ class Vertex:
 class PriorityQueue:
     def __init__(self) -> None:
         self.heap = []
-        self.removed_set = set()
     
     def push(self, priority: float, item: Vertex):
-        entry = [priority, item]
+        entry = (priority, item)
         heapq.heappush(self.heap, entry)
 
     def pop(self) -> Vertex:
         while self.heap:
             priority, entry = heapq.heappop(self.heap)
-            if entry not in self.removed_set:
-                return entry
+            return entry
     
     def remove(self, vertex: Vertex):
-        new_heap = [(priority, entry) for priority, entry in self.heap if vertex.x != entry[1].x or vertex.y != entry[1].y]
+        new_heap = [(priority, entry) for priority, entry in self.heap if vertex.x != entry.x or vertex.y != entry.y]
         heapq.heapify(new_heap)
         self.heap = new_heap
 
-    def remove_if(self, condition: Callable[[Vertex], bool]):
-        items_to_remove = [entry[1] for entry in self.heap if condition(entry[1])]
-        self.removed_set.update(items_to_remove)
-        self.heap = [entry for entry in self.heap if entry[1] not in self.removed_set]
-        heapq.heapify(self.heap)
-
 class AStar:
     def __init__(self, start: Tuple[int, int], goal: Tuple[int, int], rows, cols) -> None:        
-        self.start = Vertex(x=start[0], y=start[1], g=0, h=0)
-        self.goal = Vertex(x=goal[0], y=goal[1], g=float('inf'), h=0)
-        self.fringe = PriorityQueue()
-        self.closed = []
-        self.visited = {}
+        self.start = Vertex(start[0], start[1], self)
+        self.goal = Vertex(goal[0], goal[1], self)
+        self.open_set = PriorityQueue()
+        
+        self.came_from = {}
         self.neighbors = {}
+        self.g_score = {}
+        self.h_score = {}
+        self.f_score = {}
+
         self.grid = self.init_grid(rows, cols)
 
     def init_grid(self, rows, cols):
         return [[0 for col in range(cols)] for row in range(rows)]
 
-    def threshold_float_comparison(self, f_a, f_b) -> None:
-        THRESHOLD = .0001
-        if abs(f_a - f_b) < THRESHOLD:
-            # equality
-            return True
-        else:
-            return False
+    def reconstruct(self, current):
+        total_path = [current]
+        while current in self.came_from.keys():
+            current = self.came_from[current]
+            total_path = [current] + total_path 
+        coordinate_pairs = [(vertex.x, vertex.y) for vertex in total_path]
+        return coordinate_pairs
 
+    def calculate_d(self, current, successor) -> None:
+        dx = abs(successor.x - current.x)
+        dy = abs(successor.y - current.y)
+        
+        if dx == 1 and dy == 0:
+            return 1
+        elif dx == 0 and dy == 1:
+            return 1
+        else:
+            return math.sqrt(dx**2 + dy**2)
+
+    def calculate_h(self, succesor) -> None: 
+        self.h_score[succesor] = math.sqrt(2) * \
+        min(abs(succesor.x - self.goal.x), abs(succesor.y - self.goal.y)) + \
+        max(abs(succesor.x - self.goal.x), abs(succesor.y - self.goal.y)) - \
+        min(abs(succesor.x - self.goal.x), abs(succesor.y - self.goal.y))
+    
     def get_neighbors(self, vertex: Vertex) -> None:
         self.neighbors[vertex] = []
         y = vertex.y - 1
         if(y>=0):
-            successor = Vertex(vertex.x, y)
-            successor.calculate_g(vertex, successor)
-            successor.calculate_h(self.goal)
+            successor = Vertex(vertex.x, y, self)
             self.neighbors[vertex].append(successor)
 
         x = vertex.x + 1
         if(x < len(self.grid[0])):
-            successor = Vertex(x, vertex.y)
-            successor.calculate_g(vertex, successor)
-            successor.calculate_h(self.goal)
+            successor = Vertex(x, vertex.y, self)
             self.neighbors[vertex].append(successor)
 
         y = vertex.y + 1
         if(y < len(self.grid)):
-            successor = Vertex(vertex.x, y)
-            successor.calculate_g(vertex, successor)
-            successor.calculate_h(self.goal)
+            successor = Vertex(vertex.x, y, self)
             self.neighbors[vertex].append(successor)
     
         x = vertex.x - 1
         if(x >= 0):
-            successor = Vertex(x, vertex.y)
-            successor.calculate_g(vertex, successor)
-            successor.calculate_h(self.goal)
+            successor = Vertex(x, vertex.y, self)
             self.neighbors[vertex].append(successor)
         
         x = vertex.x + 1
         y = vertex.y - 1
         if(y >= 0 and x < len(self.grid[0])):
-            successor = Vertex(x, y)
-            successor.calculate_g(vertex, successor)
-            successor.calculate_h(self.goal)
+            successor = Vertex(x, y, self)
             self.neighbors[vertex].append(successor)
         
         x = vertex.x - 1
         y = vertex.y - 1
         if(y >= 0 and x >= 0):
-            successor = Vertex(x, y)
-            successor.calculate_g(vertex, successor)
-            successor.calculate_h(self.goal)
+            successor = Vertex(x, y, self)
             self.neighbors[vertex].append(successor)
 
         x = vertex.x + 1
         y = vertex.y + 1
         if(y < len(self.grid) and x < len(self.grid[0])):
-            successor = Vertex(x, y)
-            successor.calculate_g(vertex, successor)
-            successor.calculate_h(self.goal)
+            successor = Vertex(x, y, self)
             self.neighbors[vertex].append(successor)
 
         x = vertex.x - 1
         y = vertex.y + 1
         if(y < len(self.grid) and x >= 0):
-            successor = Vertex(x, y)
-            successor.calculate_g(vertex, successor)
-            successor.calculate_h(self.goal)
+            successor = Vertex(x, y, self)
             self.neighbors[vertex].append(successor)
 
-    def tie_break(self, successor):
-        if not self.threshold_float_comparison(successor.g, self.fringe.heap[0].g):
-            self.fringe.heap.remove(successor)
-
     def run(self) -> None:
-        self.start.h = 0
-        self.fringe.push(0, self.start)
-        while len(self.fringe.heap):
-            current = self.fringe.pop()
-            print("\n visiting ", current.x, current.y, current.g)
+        self.open_set.push(0, self.start)
+        self.g_score[self.start] = 0
+        self.h_score[self.start] = 0
+        self.f_score[self.start] = self.h_score[self.start]
+
+        while len(self.open_set.heap):
+            current = self.open_set.pop()
+            print("\n visiting ", current.x, current.y, self.g_score[current])
             # time.sleep(1)
             if (current.x == self.goal.x and current.y == self.goal.y):
-                self.closed.append(current)
                 print("Found it!")
                 # for v in self.closed:
                 #     print("\n visited", v)
-                break
-            self.closed.append(current)
-            self.fringe.remove_if(lambda n: self.visited[n] == True)
+                return self.reconstruct(current)
+            
+            self.open_set.remove(current)
+            
             self.get_neighbors(current)
+            
             print(len(self.neighbors[current]), "neighbors")
+            
             for successor in self.neighbors[current]:
-                if successor not in self.closed:
-                    verticies = [entry[1] for entry in self.fringe.heap]
-                    coordinate_pairs = [(verticy.x, verticy.y) for verticy in verticies]
-                    if (successor.x, successor.y) in coordinate_pairs:
-                        self.fringe.heap.remove(successor)
-                    self.visited[successor] = True
-                    self.fringe.push(priority=successor.g + successor.h, item=successor)                 
-                    if len(self.fringe.heap) and self.fringe.heap[0] == successor and self.threshold_float_comparison(successor.f, self.fringe.heap[0].f):
-                        self.tie_break(successor)
-                    # print(f"Added ( {successor.x}, {successor.y} ) to fringe")
-        coordinate_pairs = [(vertex.x, vertex.y) for vertex in self.closed]
-        return coordinate_pairs
+                tenative_g = self.g_score[current] + self.calculate_d(current, successor)
+                successor_g  = self.g_score[successor] if successor in self.g_score else math.inf
+                if tenative_g < successor_g:
+                    self.calculate_h(successor)
+                    self.came_from[successor] = current
+                    self.g_score[successor] = tenative_g
+                    self.f_score[successor] = tenative_g + self.h_score[successor]
+                    if successor not in self.open_set.heap:
+                        self.open_set.push(self.g_score[successor], successor)
     
 class AStarBlocked(AStar):
     def __init__(self, start: Tuple[int, int], goal: Tuple[int, int], rows, cols) -> None:
@@ -228,7 +207,7 @@ class AStarBlocked(AStar):
         north_west_blocked = is_blocked(x, y)
         if y >= 0 and x >= 0 and not north_west_blocked:
             north_west_successor = Vertex(x, y)
-            north_west_successor.calculate_g(vertex, north_west_successor)
+            north_west_successor.calculate_d(vertex, north_west_successor)
             north_west_successor.calculate_h(self.goal)
             self.neighbors[vertex].append(north_west_successor)
         
@@ -238,7 +217,7 @@ class AStarBlocked(AStar):
         north_blocked = is_blocked(x, y) if y > 0 else True
         if y >= 0 and not (north_west_blocked and north_blocked) and x < len(self.grid[0]) - 1:
             north_successor = Vertex(x, y)
-            north_successor.calculate_g(vertex, north_successor)
+            north_successor.calculate_d(vertex, north_successor)
             north_successor.calculate_h(self.goal)
             self.neighbors[vertex].append(north_successor)
 
@@ -247,7 +226,7 @@ class AStarBlocked(AStar):
         east_successor = None
         if x < len(self.grid[0]) and not (self_blocked and north_blocked) and not (north_blocked and y == len(self.grid) -1):
             east_successor = Vertex(x, y)
-            east_successor.calculate_g(vertex, east_successor)
+            east_successor.calculate_d(vertex, east_successor)
             east_successor.calculate_h(self.goal)
             self.neighbors[vertex].append(east_successor)
 
@@ -257,7 +236,7 @@ class AStarBlocked(AStar):
         west_blocked = is_blocked(x, y)
         if x >= 0 and (not north_west_blocked and y < len(self.grid) - 1) and not (west_blocked and north_west_blocked):
             west_successor = Vertex(x, y)
-            west_successor.calculate_g(vertex, west_successor)
+            west_successor.calculate_d(vertex, west_successor)
             west_successor.calculate_h(self.goal)
             self.neighbors[vertex].append(west_successor)
         
@@ -266,7 +245,7 @@ class AStarBlocked(AStar):
         south_successor = None
         if y < len(self.grid) and not (self_blocked and west_blocked):
             south_successor = Vertex(x, y)
-            south_successor.calculate_g(vertex, south_successor)
+            south_successor.calculate_d(vertex, south_successor)
             south_successor.calculate_h(self.goal)
             self.neighbors[vertex].append(south_successor)
         
@@ -275,7 +254,7 @@ class AStarBlocked(AStar):
         north_east_successor = None
         if y >= 0 and x < len(self.grid[0]) and not north_blocked:
             north_east_successor = Vertex(x, y)
-            north_east_successor.calculate_g(vertex, north_east_successor)
+            north_east_successor.calculate_d(vertex, north_east_successor)
             north_east_successor.calculate_h(self.goal)
             self.neighbors[vertex].append(north_east_successor)
 
@@ -284,7 +263,7 @@ class AStarBlocked(AStar):
         south_east_successor = None
         if y < len(self.grid) and x < len(self.grid[0]) and not self_blocked:
             south_east_successor = Vertex(x, y)
-            south_east_successor.calculate_g(vertex, south_east_successor)
+            south_east_successor.calculate_d(vertex, south_east_successor)
             south_east_successor.calculate_h(self.goal)
             self.neighbors[vertex].append(south_east_successor)
         
@@ -293,15 +272,15 @@ class AStarBlocked(AStar):
         south_west_successor = None
         if y < len(self.grid) and x >= 0 and not west_blocked:
             south_west_successor = Vertex(x, y)
-            south_west_successor.calculate_g(vertex, south_west_successor)
+            south_west_successor.calculate_d(vertex, south_west_successor)
             south_west_successor.calculate_h(self.goal)
             self.neighbors[vertex].append(south_west_successor)
 
         print("blocked ", self.blocked)
         # print("is self blocked", self_blocked)
         
-        # for vertex in self.neighbors[vertex]:
-        #     print(vertex.x, vertex.y)
+        for vertex in self.neighbors[vertex]:
+            print(vertex.x, vertex.y, vertex.g)
 
     def run(self):
         coordinate_pairs = super().run()
